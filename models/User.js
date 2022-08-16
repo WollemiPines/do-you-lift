608b30787bd801cd7edb4e53da921b0fe6035302
Alex <alexander.h.perry@gmail.com>
2022-08-16 21:13:21 +1000

    added bcrypt

--- models/User.js
+++ models/User.js
@@ -1,8 +1,13 @@
-const { DataTypes } = require('sequelize/types');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 const Workout = require('./Workout');
 
-class User extends Model { }
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+};
 
 User.init(
     {
