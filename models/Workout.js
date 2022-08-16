99abe244f6a53ac043a74e65a096527207e81cb8
Alex <alexander.h.perry@gmail.com>
2022-08-16 21:29:23 +1000

    added workout and category models

---
+++ models/Workout.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Workout extends Model { }
+
+Workout.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { isAlphanumeric: true }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        reps: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelname: 'workout'
+    }
+);
---
+++ models/category.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Category extends Model { }
+
+Category.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelname: 'category'
+    }
+);
