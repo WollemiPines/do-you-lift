cae11b4bfc02264fb806748d22c19250ceb103b3
brake <riaconoo@icloud.com>
2022-08-18 19:49:15 +1000

    updated cookie session data, stored secret in dotenv file

--- server.js
+++ server.js
@@ -12,9 +12,24 @@ const PORT = process.env.PORT || 3001;
 // Add cookie sesion data /
 // ///////////////////////
 
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
+app.use(session(sess))
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes)
 
