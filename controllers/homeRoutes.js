363cd38ddb7d37d6f0070e117c6eb8b9a991f108
brake <riaconoo@icloud.com>
2022-08-18 20:24:07 +1000

    added routes for workouts and user profile page

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { User, Workout, Category} = require('../models');
+const withAuth = require('../utils/auth');
+
+// Route to user's workout page
+router.get('/workouts', async (req, res) => {
+    try {
+        const workoutData = await Workout.findAll({
+            include: [
+                {
+                    model: Category,
+                    attributes: ['name']
+                },
+            ],
+        });
+
+        const workouts = workoutData.map((category) => category.get({ plain: true }));
+
+        res.render('workouts', {
+            workouts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+// Route to user's profile page
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user.id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Workout}]
+        });
+
+        const user = userData.get({ plain: true});
+
+        res.render('userprofile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+// If the user is already logged in, redirect the request to their profile.
+router.get('/login', (req, res) => {
+    if (req.session.logged_in){
+        res.redirect('/userprofile');
+        return
+    }
+    res.render('login')
+})
+
+module.exports = router;
