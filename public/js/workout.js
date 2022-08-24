3123df9662acfed988be27c3198e8c914cd9edfd
Lukesudom <lukemarshall511@gmail.com>
2022-08-24 22:05:44 +1000

    selecting handlebars object

--- public/js/workout.js
+++ public/js/workout.js
@@ -1,4 +1,20 @@
 
-    const workoutName = document.querySelector('.workout-name').value;
-    const workoutReps = document.querySelector('.workout-reps').value;
-    
+   
+
+    var workoutReps = parseInt(document.querySelector('#workout-0'));
+
+    const workoutIn = document.querySelector('.workout-in');
+    const workoutDec = document.querySelector('.workout-dec');
+
+    console.log(JSON.stringify(workoutReps));
+
+    // workoutIn.addEventListener('click', function(){
+    //     workoutReps = parseInt(workoutReps) +1;
+
+    //     console.log('btn clicked')
+    //     console.log(workoutReps);
+    // });
+
+
+
+
