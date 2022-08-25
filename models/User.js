3d0deb5196200f13e035bd7acc14ca8566e6259f
r-r-i <104706800+r-r-i@users.noreply.github.com>
2022-08-25 18:33:58 +1000

    Revert "67 when user logs a workout post data to userworkouts 1"

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-const UserWorkouts = require('./userWorkouts');
 
 class User extends Model {
     checkPassword(loginPw) {
